3ae58d1735ecdfb96d1327facce306671ac9353c
ximera2888 <ximera2888@gmail.com>
2024-09-14 23:02:54 +0500

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.9.24"
+}
+
+group = "ru.netology"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(8)
+}
